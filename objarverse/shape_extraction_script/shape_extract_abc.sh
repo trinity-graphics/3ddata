d0dea6b8a2daf8e80c21a6d5047197013181f70a
KhoiDOO <khoido8899@gmail.com>
2025-10-15 22:05:11 +0000

    update objarverse shape extraction and cleaning for abc file_ext

---
+++ objarverse/shape_extraction_script/shape_extract_abc.sh
@@ -0,0 +1,12 @@
+INPUT_DIR="/mnt/raw/objarverse_raw"
+OUTPUT_DIR="/mnt/shape/objarverse_shape"
+
+cd ..
+
+for chunk_index in {0..0}
+do 
+    python3 shape_extract.py \
+        --input_dir "${INPUT_DIR}/obj/github/chunk_${chunk_index}" \
+        --output_dir "${OUTPUT_DIR}/obj/github/chunk_${chunk_index}" \
+        --file_ext abc
+done
