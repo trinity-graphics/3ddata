3c75d0349ee190e052f382f7b123fd464c0cf6fc
KhoiDOO <khoido8899@gmail.com>
2025-10-15 15:57:31 +0000

    update shape extraction for glb in objarverse

---
+++ objarverse/shape_clean_script/shape_clean_glb.sh
@@ -0,0 +1,12 @@
+INPUT_DIR="/mnt/raw/objarverse_shape"
+OUTPUT_DIR="/mnt/shape/objarverse_clean"
+
+cd ..
+
+for chunk_index in {0..810}
+do 
+    python3 shape_clean.py \
+        --input_dir "${INPUT_DIR}/glb/github/chunk_${chunk_index}" \
+        --output_dir "${OUTPUT_DIR}/glb/github/chunk_${chunk_index}" \
+        --file_ext glb
+done
