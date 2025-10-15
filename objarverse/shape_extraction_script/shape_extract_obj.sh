33b22b6baca27a3df60713881209791218a042a7
KhoiDOO <khoido8899@gmail.com>
2025-10-15 09:11:51 +0000

    hotfix

--- objarverse/shape_extraction_script/shape_extract_obj.sh
+++ objarverse/shape_extraction_script/shape_extract_obj.sh
@@ -5,7 +5,7 @@ cd ..
 
 for chunk_index in {0..810}
 do 
-    python3 shape_extract \
+    python3 shape_extract.py \
         --input_dir "${INPUT_DIR}/obj/github/chunk_{$chunk_index}" \
         --output_dir "${OUTPUT_DIR}/obj/github/chunk_{$chunk_index}" \
         --file_ext obj
