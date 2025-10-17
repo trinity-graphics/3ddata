1004e33a409433dbd9ca9549a3ea178da22c16b4
KhoiDOO <khoido8899@gmail.com>
2025-10-17 14:46:50 +0000

    update shape extraction and cleaning script

---
+++ objarverse/shape_clean_script/shape_clean_dae.sh
@@ -0,0 +1,11 @@
+INPUT_DIR="/mnt/shape/objarverse_shape"
+OUTPUT_DIR="/mnt/clean/objarverse_clean"
+
+cd ..
+
+for chunk_index in {0..61}
+do 
+    python3 shape_clean.py \
+        --input_dir "${INPUT_DIR}/dae/github/chunk_${chunk_index}" \
+        --output_dir "${OUTPUT_DIR}/dae/github/chunk_${chunk_index}"
+done
--- objarverse/shape_extraction_script/shape_extract_dae.sh
+++ objarverse/shape_extraction_script/shape_extract_dae.sh
@@ -3,7 +3,7 @@ OUTPUT_DIR="/mnt/shape/objarverse_shape"
 
 cd ..
 
-for chunk_index in {0..0}
+for chunk_index in {0..61}
 do 
     python3 shape_extract.py \
         --input_dir "${INPUT_DIR}/dae/github/chunk_${chunk_index}" \
