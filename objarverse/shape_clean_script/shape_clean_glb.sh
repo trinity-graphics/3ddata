1cccd226810d5ac855a1aae8fa82f0cf5e564187
KhoiDOO <khoido8899@gmail.com>
2025-10-17 09:32:32 +0000

    update the number of chunks for glb cleaning

--- objarverse/shape_clean_script/shape_clean_glb.sh
+++ objarverse/shape_clean_script/shape_clean_glb.sh
@@ -3,7 +3,7 @@ OUTPUT_DIR="/mnt/shape/objarverse_clean"
 
 cd ..
 
-for chunk_index in {0..810}
+for chunk_index in {0..492}
 do 
     python3 shape_clean.py \
         --input_dir "${INPUT_DIR}/glb/github/chunk_${chunk_index}" \
