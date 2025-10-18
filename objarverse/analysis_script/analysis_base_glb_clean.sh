9dc4345a74fe1b15783aab88bb8255ff530b2ef5
KhoiDOO <khoido8899@gmail.com>
2025-10-18 14:17:05 +0000

    update analysis for glb

---
+++ objarverse/analysis_script/analysis_base_glb_clean.sh
@@ -0,0 +1,11 @@
+INPUT_DIR="/mnt/clean/objarverse_clean"
+OUTPUT_DIR="/mnt/clean/objarverse_analysis"
+
+cd ../analysis
+
+for chunk_index in {0..492}
+do 
+    python3 base.py \
+        --folder_path "${INPUT_DIR}/glb/github/chunk_${chunk_index}" \
+        --output_path "${OUTPUT_DIR}/glb/github/chunk_${chunk_index}.csv"
+done
