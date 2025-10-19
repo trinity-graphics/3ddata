01d6a12f2a15cab2c6372cfdfcf4769484e26d8a
KhoiDOO <khoido8899@gmail.com>
2025-10-19 15:42:51 +0000

    update fbx cleaning script

---
+++ objarverse/shape_clean_script/shape_clean_fbx_batch.sh
@@ -0,0 +1,15 @@
+NUM_PARALLEL=${1:-4}
+
+INPUT_DIR="/mnt/shape/objarverse_shape"
+OUTPUT_DIR="/mnt/clean/objarverse_clean"
+
+cd ..
+
+echo "Running shape_clean.py with ${NUM_PARALLEL} parallel jobs..."
+
+seq 0 754 | xargs -n 1 -P "${NUM_PARALLEL}" -I {} \
+python3 shape_clean.py \
+    --input_dir "${INPUT_DIR}/gltf/github/chunk_{}" \
+    --output_dir "${OUTPUT_DIR}/gltf/github/chunk_{}"
+
+echo "All chunks processed."
---
+++ objarverse/shape_extraction_script/shape_extract_blend_batch.sh
@@ -0,0 +1,15 @@
+NUM_PARALLEL=${1:-4}
+
+INPUT_DIR="/mnt/raw/objarverse_raw"
+OUTPUT_DIR="/mnt/shape/objarverse_shape"
+
+cd ..
+
+echo "Running shape_extract.py with ${NUM_PARALLEL} parallel jobs..."
+
+seq 0 810 | xargs -n 1 -P "${NUM_PARALLEL}" -I {} \
+python3 shape_extract.py \
+    --input_dir "${INPUT_DIR}/blend/github/chunk_{}" \
+    --output_dir "${OUTPUT_DIR}/blend/github/chunk_{}" \
+    --file_ext blend
+echo "All chunks processed."
