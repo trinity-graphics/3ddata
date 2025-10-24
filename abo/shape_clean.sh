b011483f2bdd020545058fc9a754d536f855cf5d
KhoiDOO <khoido8899@gmail.com>
2025-10-24 11:21:05 +0000

    update cleaning for abo

--- abo/shape_clean.sh
+++ abo/shape_clean.sh
@@ -1,12 +1,15 @@
 NUM_PARALLEL=${1:-16}
 
-INPUT_DIR="/mnt/shape/shapenet_shape"
-OUTPUT_DIR="/mnt/clean/shapenet_clean"
+INPUT_DIR="/mnt/shape/abo/3dmodels/obj"
+OUTPUT_DIR="/mnt/clean/abo/3dmodels/obj"
 
-cd ../objarverse
+echo "Running shape_clean.py with ${NUM_PARALLEL} parallel jobs..."
 
-echo "Running shape_extract.py with ${NUM_PARALLEL} parallel jobs..."
+CHUNKS=({0..9} {A..Z})
 
+printf "%s\n" "${CHUNKS[@]}" | xargs -n 1 -P "${NUM_PARALLEL}" -I {} \
 python3 shape_clean.py \
-    --input_dir "${INPUT_DIR}" \
-    --output_dir "${OUTPUT_DIR}"
+    --input_dir "${INPUT_DIR}/{}" \
+    --output_dir "${OUTPUT_DIR}/{}"
+
+echo "All chunks processed."
